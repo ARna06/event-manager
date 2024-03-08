485be6f0550a10d6c0264c0b74dc7cfeab8d3005
ARna06 <72038543+Leopard005537@users.noreply.github.com>
2024-03-09 01:42:54 +0530

    Solved Exercises

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,29 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
+require 'date'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def clean_phone_number(number)
+  if (number.to_s.chars.length === 11) && number.to_s[0] ===1
+    return number.to_s[1..10]
+  elsif number.to_s.chars.length != 10
+    return ''
+  else return number
+  end
+end
+
+def peak_evaluator(arr)
+  tally = arr.reduce Hash.new(0) do |prev, item|
+    prev[item.to_s] += 1
+    prev
+  end
+  max_hours = tally.map {|k, v| k if v === tally.values.max}
+  return max_hours.select { |item| item.to_s != '' }
 end
 
 def legislators_by_zipcode(zip)
@@ -39,6 +59,8 @@ contents = CSV.open(
 
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
+hours = []
+week_days = []
 
 contents.each do |row|
   id = row[0]
@@ -46,9 +68,18 @@ contents.each do |row|
 
   zipcode = clean_zipcode(row[:zipcode])
 
+  phone_number = clean_phone_number(row[:homephone])
+
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
   save_thank_you_letter(id,form_letter)
+
+  hours.push(Time.strptime(row[:regdate].to_s, "%m/%d/%Y %k:%M").hour)
+  week_days.push(Date::DAYNAMES[Date.strptime(row[:regdate].to_s, "%m/%d/%Y %k:%M").wday])
+
 end
+
+puts "The peak registration time is #{peak_evaluator(hours).join(', ')} hours respectively."
+puts "The peak registration weekday is #{peak_evaluator(week_days).join(', ')} respectively."
